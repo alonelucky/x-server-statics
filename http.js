4166a44d17f24b3760f61ac04507934e6181800f
xiaoyiyu <xiaqiubo123@163.com>
2017-07-17 09:59:48 +0800

    结合mime文件,完成整体封装

--- http.js
+++ http.js
@@ -5,45 +5,48 @@
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
+const mime = require('./mime.json');
 
-const mime = {
-    '.html':'text/html',
-    '.css':'text/css'
-}
 
 module.exports=serverStatic;
 
 function serverStatic(){
     // 获取连接的path部分
-    let src = url.parse(arguments[0].url).pathname;
+    let src = url.parse(arguments[0][0].url).pathname;
     // 获取path部分的文件后缀
     let extname = path.extname(src);
     // 如果文件扩展名存在则执行文件读取输出
     if(extname){
         // 读取对应路径下的文件
-        fs.readFile(arguments[2]+'/'+src,(err,data)=>{
+        fs.readFile(arguments[1]+'/'+src,(err,data)=>{
             // 不存在返回404
             if(err){
-                arguments[1].writeHead(404,{'content-type':mime[extname]});
-                arguments[1].end('Not Found');
+                arguments[0][1].writeHead(404,{'content-type':mime[extname]});
+                arguments[0][1].end('Not Found');
                 return;
             }
             // 存在返回数据
-            arguments[1].writeHead(200,{'content-type':mime[extname]});
-            arguments[1].end(data);
+            arguments[0][1].writeHead(200,{'content-type':mime[extname]});
+            arguments[0][1].end(data);
+            return;
         });
     }else{
         // 如果没有文件扩展名,则按文件夹处理,读取改文件夹下的index.html文件
-        fs.readFile(arguments[2]+'/'+src+'/index.html',(err,data)=>{
-            // 如果不存在,返回404
+        fs.readFile(arguments[1]+'/'+src+'/index.html',(err,data)=>{
+            // 如果不存在,则执行next中间件函数
             if(err){
-                arguments[1].writeHead(404,{'content-type':mime[extname]});
-                arguments[1].end('Not Found');
+                if(arguments[0][2]){
+                    arguments[0][2]();
+                }
                 return;
             }
             // 如果存在,返回数据
-            arguments[1].writeHead(200,{'content-type':mime[extname]});
-            arguments[1].end(data);
+            arguments[0][1].writeHead(200,{'content-type':mime['.html']});
+            arguments[0][1].end(data);
+            return;
         });
+    }
+    if(arguments[2]){
+        arguments[2]();
     }
 }
